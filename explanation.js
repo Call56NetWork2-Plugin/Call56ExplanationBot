fde811d600ec4a6219e82f8e9702d8fc65957eff
Kinoko_2k <65443692+kinoko2k@users.noreply.github.com>
2021-08-07 18:51:51 +0900

    Update explanation.js

--- explanation.js
+++ explanation.js
@@ -1,1 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({
+  disableMentions: 'everyone'
+})
+const http = require("http"); // npm i http
+const settings = {
+  prefix: 'c!',
+  token: 'TOKEN'
+};
+const prefix = 'c!'
 
+client.once("ready", () => {
+  console.log("Ready!");
+});
